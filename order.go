9fd4a94443b06a5cf000398867497a419a975316
Andrew Wormald <andreww@luno.com>
2024-04-19 20:03:09 +0100

    DRAFT: workflow: Add list to store with pagination

---
+++ order.go
@@ -0,0 +1,22 @@
+package workflow
+
+type OrderType int
+
+const (
+	OrderTypeUnknown    OrderType = 0
+	OrderTypeAscending  OrderType = 1
+	OrderTypeDescending OrderType = 2
+	sentinelOrderType   OrderType = 3
+)
+
+func (ot *OrderType) String() string {
+	switch *ot {
+	case OrderTypeAscending:
+		return "asc"
+
+	case OrderTypeDescending:
+		return "desc"
+	default:
+		return ""
+	}
+}
