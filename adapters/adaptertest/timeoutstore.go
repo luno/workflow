899d5debbfe2aceb0c6e41f15e1c1d7be9c437ff
Andrew Wormald <andreww@luno.com>
2024-09-16 11:54:20 +0100

    reduce code duplication

--- adapters/adaptertest/timeoutstore.go
+++ adapters/adaptertest/timeoutstore.go
@@ -2,6 +2,7 @@ package adaptertest
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -35,53 +36,29 @@ func testCancelTimeout(t *testing.T, factory func() workflow.TimeoutStore) {
 	err = store.Create(ctx, "example", "andrew", "3", int(statusStarted), time.Now().Add(-time.Hour))
 	require.Nil(t, err)
 
-	timeout, err := store.ListValid(ctx, "example", int(statusStarted), time.Now())
+	timeouts, err := store.ListValid(ctx, "example", int(statusStarted), time.Now())
 	require.Nil(t, err)
 
-	require.Equal(t, 3, len(timeout))
+	require.Equal(t, 3, len(timeouts))
 
-	require.Equal(t, "example", timeout[0].WorkflowName)
-	require.Equal(t, "andrew", timeout[0].ForeignID)
-	require.Equal(t, "1", timeout[0].RunID)
-	require.False(t, timeout[0].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[0].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[0].CreatedAt, time.Second)
-
-	require.Equal(t, "example", timeout[1].WorkflowName)
-	require.Equal(t, "andrew", timeout[1].ForeignID)
-	require.Equal(t, "2", timeout[1].RunID)
-	require.False(t, timeout[1].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[1].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[1].CreatedAt, time.Second)
-
-	require.Equal(t, "example", timeout[2].WorkflowName)
-	require.Equal(t, "andrew", timeout[2].ForeignID)
-	require.Equal(t, "3", timeout[2].RunID)
-	require.False(t, timeout[2].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[2].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[2].CreatedAt, time.Second)
+	for i, timeout := range timeouts {
+		require.Equal(t, "example", timeout.WorkflowName)
+		require.Equal(t, "andrew", timeout.ForeignID)
+		require.Equal(t, fmt.Sprintf("%v", i+1), timeout.RunID)
+		require.False(t, timeout.Completed)
+		require.WithinDuration(t, time.Now().Add(-time.Hour), timeout.ExpireAt, time.Second)
+		require.WithinDuration(t, time.Now(), timeout.CreatedAt, time.Second)
+	}
 
 	err = store.Cancel(ctx, 2)
 	require.Nil(t, err)
 
-	timeout, err = store.ListValid(ctx, "example", int(statusStarted), time.Now())
+	timeouts, err = store.ListValid(ctx, "example", int(statusStarted), time.Now())
 	require.Nil(t, err)
 
-	require.Equal(t, 2, len(timeout))
-
-	require.Equal(t, "example", timeout[0].WorkflowName)
-	require.Equal(t, "andrew", timeout[0].ForeignID)
-	require.Equal(t, "1", timeout[0].RunID)
-	require.False(t, timeout[0].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[0].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[0].CreatedAt, time.Second)
-
-	require.Equal(t, "example", timeout[1].WorkflowName)
-	require.Equal(t, "andrew", timeout[1].ForeignID)
-	require.Equal(t, "3", timeout[1].RunID)
-	require.False(t, timeout[1].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[1].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[1].CreatedAt, time.Second)
+	require.Equal(t, 2, len(timeouts))
+	require.Equal(t, "1", timeouts[0].RunID)
+	require.Equal(t, "3", timeouts[1].RunID)
 }
 
 func testCompleteTimeout(t *testing.T, factory func() workflow.TimeoutStore) {
@@ -97,53 +74,29 @@ func testCompleteTimeout(t *testing.T, factory func() workflow.TimeoutStore) {
 	err = store.Create(ctx, "example", "andrew", "3", int(statusStarted), time.Now().Add(-time.Hour))
 	require.Nil(t, err)
 
-	timeout, err := store.ListValid(ctx, "example", int(statusStarted), time.Now())
+	timeouts, err := store.ListValid(ctx, "example", int(statusStarted), time.Now())
 	require.Nil(t, err)
 
-	require.Equal(t, 3, len(timeout))
+	require.Equal(t, 3, len(timeouts))
 
-	require.Equal(t, "example", timeout[0].WorkflowName)
-	require.Equal(t, "andrew", timeout[0].ForeignID)
-	require.Equal(t, "1", timeout[0].RunID)
-	require.False(t, timeout[0].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[0].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[0].CreatedAt, time.Second)
-
-	require.Equal(t, "example", timeout[1].WorkflowName)
-	require.Equal(t, "andrew", timeout[1].ForeignID)
-	require.Equal(t, "2", timeout[1].RunID)
-	require.False(t, timeout[1].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[1].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[1].CreatedAt, time.Second)
-
-	require.Equal(t, "example", timeout[2].WorkflowName)
-	require.Equal(t, "andrew", timeout[2].ForeignID)
-	require.Equal(t, "3", timeout[2].RunID)
-	require.False(t, timeout[2].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[2].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[2].CreatedAt, time.Second)
+	for i, timeout := range timeouts {
+		require.Equal(t, "example", timeout.WorkflowName)
+		require.Equal(t, "andrew", timeout.ForeignID)
+		require.Equal(t, fmt.Sprintf("%v", i+1), timeout.RunID)
+		require.False(t, timeout.Completed)
+		require.WithinDuration(t, time.Now().Add(-time.Hour), timeout.ExpireAt, time.Second)
+		require.WithinDuration(t, time.Now(), timeout.CreatedAt, time.Second)
+	}
 
 	err = store.Complete(ctx, 2)
 	require.Nil(t, err)
 
-	timeout, err = store.ListValid(ctx, "example", int(statusStarted), time.Now())
+	timeouts, err = store.ListValid(ctx, "example", int(statusStarted), time.Now())
 	require.Nil(t, err)
 
-	require.Equal(t, 2, len(timeout))
-
-	require.Equal(t, "example", timeout[0].WorkflowName)
-	require.Equal(t, "andrew", timeout[0].ForeignID)
-	require.Equal(t, "1", timeout[0].RunID)
-	require.False(t, timeout[0].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[0].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[0].CreatedAt, time.Second)
-
-	require.Equal(t, "example", timeout[1].WorkflowName)
-	require.Equal(t, "andrew", timeout[1].ForeignID)
-	require.Equal(t, "3", timeout[1].RunID)
-	require.False(t, timeout[1].Completed)
-	require.WithinDuration(t, time.Now().Add(-time.Hour), timeout[1].ExpireAt, time.Second)
-	require.WithinDuration(t, time.Now(), timeout[1].CreatedAt, time.Second)
+	require.Equal(t, 2, len(timeouts))
+	require.Equal(t, "1", timeouts[0].RunID)
+	require.Equal(t, "3", timeouts[1].RunID)
 }
 
 func testListTimeout(t *testing.T, factory func() workflow.TimeoutStore) {
