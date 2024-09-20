f0798928eeb4223d6bca2c4452fa507be9f60c12
Andrew Wormald <andreww@luno.com>
2024-09-20 22:47:16 +0100

    workflow: Support json.Marshaler implementations for Require

--- unmarshal.go
+++ unmarshal.go
@@ -4,10 +4,5 @@ import "encoding/json"
 
 // Unmarshal create a single point of change if the decoding changes.
 func Unmarshal[T any](b []byte, t *T) error {
-	err := json.Unmarshal(b, t)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return json.Unmarshal(b, t)
 }
