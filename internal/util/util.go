827d3b7e7c8f98b265d357ad717a151ac108d142
Andrew Wormald <andreww@luno.com>
2024-09-23 17:26:17 +0100

    visualiser: Allow spacing in node names

--- internal/util/util.go
+++ internal/util/util.go
@@ -5,11 +5,10 @@ import "unicode"
 func CamelToSpacing(s string) string {
 	var result []rune
 	for i, r := range s {
-		// If the character is uppercase and it's not the first character, add a space before it.
 		if unicode.IsUpper(r) && i > 0 {
 			result = append(result, ' ')
 		}
-		// Append the current character (it will preserve its case).
+
 		result = append(result, r)
 	}
 	return string(result)
