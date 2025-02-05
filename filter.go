98deb765e9d075f9a534c77c713a79b69471e843
Andrew Wormald <andreww@luno.com>
2025-02-05 14:16:16 +0000

    recordstore/filter: Convert int64 param to any underlying int variation  (#90)
    * recordstore/filter: Change from int param to any underlying int (variations) type
    
    * add int8 and int16

--- filter.go
+++ filter.go
@@ -1,6 +1,8 @@
 package workflow
 
-import "strconv"
+import (
+	"strconv"
+)
 
 func MakeFilter(filters ...RecordFilter) *recordFilters {
 	var rf recordFilters
@@ -49,9 +51,9 @@ func FilterByForeignID(val string) RecordFilter {
 	}
 }
 
-func FilterByStatus(status int64) RecordFilter {
+func FilterByStatus[statusType ~int | ~int8 | ~int16 | ~int32 | ~int64](status statusType) RecordFilter {
 	return func(filters *recordFilters) {
-		i := strconv.FormatInt(status, 10)
+		i := strconv.FormatInt(int64(status), 10)
 		filters.byStatus = makeFilterValue(i)
 	}
 }
