00c152ee23829e9c0fba7cd5b5265fee67976dec
Andrew Wormald <andreww@luno.com>
2025-12-12 15:59:59 +0000

    adapters/redis: Rename to wredis

--- adapters/wredis/store_test.go
+++ adapters/wredis/store_test.go
@@ -3,12 +3,13 @@ package redis
 import (
 	"testing"
 
-	"github.com/luno/workflow"
-	"github.com/luno/workflow/adapters/adaptertest"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	rediscontainer "github.com/testcontainers/testcontainers-go/modules/redis"
+
+	"github.com/luno/workflow"
+	"github.com/luno/workflow/adapters/adaptertest"
 )
 
 func TestRedisRecordStore(t *testing.T) {
